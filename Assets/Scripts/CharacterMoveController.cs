c2ee15e10bc40235efb41e52141f4afb8ab86989
Alyanisa Elsa Ayu Anandia <90304203+alyanisaelsa@users.noreply.github.com>
2021-09-15 22:54:10 +0700

    Initial commit

---
+++ Assets/Scripts/CharacterMoveController.cs
@@ -0,0 +1,139 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMoveController : MonoBehaviour
+{
+    //untuk mengatur akselerasi dan kecepatan maksimum
+    [Header("Movement")]
+    public float moveAccel;
+    public float maxSpeed;
+
+    //untuk mengontrol kamera bisa mengikuti si karakter
+    [Header("Camera")]
+    public CameraMoveController gameCamera;
+
+    //karakter bisa loncat
+    [Header("Jump")]
+    public float jumpAccel;
+
+    [Header("Ground Raycast")]
+    public float groundRaycastDistance;
+    public LayerMask groundLayerMask;
+
+    //score
+    [Header("Scoring")]
+    public ScoreController score;
+    public float scoringRatio;
+
+    //game over
+    [Header("GameOver")]
+    public GameObject gameOverScreen;
+    public float fallPositionY;
+
+    private bool isJumping;
+    private bool isOnGround;
+    private float lastPositionX;
+
+    private Rigidbody2D rig;
+    private Animator anim;
+    private CharacterSoundController sound;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rig = GetComponent<Rigidbody2D>();
+
+        anim = GetComponent<Animator>();
+        sound = GetComponent<CharacterSoundController>();
+
+        lastPositionX = transform.position.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // read input
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (isOnGround)
+            {
+                isJumping = true;
+
+                sound.PlayJump();
+            }
+        }
+
+        // change animation
+        anim.SetBool("isOnGround", isOnGround);
+
+        // calculate score
+        int distancePassed = Mathf.FloorToInt(transform.position.x - lastPositionX);
+        int scoreIncrement = Mathf.FloorToInt(distancePassed / scoringRatio);
+
+        if (scoreIncrement > 0)
+        {
+            score.IncreaseCurrentScore(scoreIncrement);
+            lastPositionX += distancePassed;
+        }
+
+        // game over
+        if (transform.position.y < fallPositionY)
+        {
+            GameOver();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        // menambahkan raycast ground agar bisa diubah sesuai keinginan
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, groundRaycastDistance, groundLayerMask);
+        if (hit)
+        {
+            if (!isOnGround && rig.velocity.y <= 0)
+            {
+                isOnGround = true;
+            }
+        }
+        else
+        {
+            isOnGround = false;
+        }
+
+        Vector2 velocityVector = rig.velocity;
+
+        if (isJumping)
+        {
+            velocityVector.y += jumpAccel;
+            isJumping = false;
+        }
+
+        velocityVector.x = Mathf.Clamp(velocityVector.x + moveAccel * Time.deltaTime, 0.0f, maxSpeed);
+
+
+        rig.velocity = velocityVector;
+
+    }
+
+    private void OnDrawGizmos()
+    {
+        Debug.DrawLine(transform.position, transform.position + (Vector3.down * groundRaycastDistance), Color.white);
+    }
+    
+    private void GameOver()
+    {
+        // set high score
+        score.FinishScoring();
+
+        // stop camera movement
+        gameCamera.enabled = false;
+
+        // show gameover
+        gameOverScreen.SetActive(true);
+
+        // disable this too
+        this.enabled = false;
+    }
+
+ 
+}
